23cf27601b8f684c16c1d1f5f836f724a69785c3
numselli <58607232+numselli@users.noreply.github.com>
2021-09-12 16:53:16 -0700

    add support for more sites

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 const fetch = require('node-fetch'), randomstring = require('randomstring'), config = require('./config.json');
 
-setInterval(async() => fetch(config.discordWebhook, {method: 'POST',headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({"content": `https://prnt.sc/${randomstring.generate({length: 6, charset:'alphanumeric'}).toLowerCase()}`})}), config.interval)
+const sites = [
+	{
+		"url": "https://prnt.sc/++code++",
+		"codeLen": 6,
+		"codeType": "alphanumeric",
+		"lowerCase": true
+	},
+	{
+		"url": "https://postimg.cc/++code++",
+		"codeLen": 8,
+		"codeType": "alphanumeric",
+		"lowerCase": false
+	},
+	{
+		"url": "https://imgpile.com/i/++code++",
+		"codeLen": 6,
+		"codeType": "alphanumeric",
+		"lowerCase": false
+	},
+	{
+		"url": " https://ibb.co/++code++",
+		"codeLen": 7,
+		"codeType": "alphanumeric",
+		"lowerCase": false
+	}
+]
+
+setInterval(async() => {
+	const site = sites[Math.floor((Math.random() * Object.keys(sites).length))];
+	const siteURL = site.url.replace('++code++', randomstring.generate({length: Math.floor((Math.random() * site.codeLen)+1), charset:site.codeType}))
+	fetch(config.discordWebhook, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			"content": site.lowerCase ? siteURL.toLowerCase() : siteURL
+		})
+	})
+}, config.interval)

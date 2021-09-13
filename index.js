abcd8d1e122a08cee37180b431829e05b50fe19c
numselli <58607232+numselli@users.noreply.github.com>
2021-09-12 17:26:56 -0700

    improve random code

--- index.js
+++ index.js
@@ -4,32 +4,36 @@ const sites = [
 	{
 		"url": "https://prnt.sc/++code++",
 		"codeLen": 6,
+		"minCodeLen": 5,
 		"codeType": "alphanumeric",
 		"lowerCase": true
 	},
 	{
 		"url": "https://postimg.cc/++code++",
 		"codeLen": 8,
+		"minCodeLen": 7,
 		"codeType": "alphanumeric",
 		"lowerCase": false
 	},
 	{
 		"url": "https://imgpile.com/i/++code++",
 		"codeLen": 6,
+		"minCodeLen": 6,
 		"codeType": "alphanumeric",
 		"lowerCase": false
 	},
 	{
 		"url": " https://ibb.co/++code++",
 		"codeLen": 7,
+		"minCodeLen": 2,
 		"codeType": "alphanumeric",
 		"lowerCase": false
 	}
 ]
 
 setInterval(async() => {
-	const site = sites[Math.floor((Math.random() * Object.keys(sites).length))];
-	const siteURL = site.url.replace('++code++', randomstring.generate({length: Math.floor((Math.random() * site.codeLen)+1), charset:site.codeType}))
+	const site = sites[getRandomNum(0, Object.keys(sites).length)];
+	const siteURL = site.url.replace('++code++', randomstring.generate({length: getRandomNum(site.minCodeLen, site.codeLen), charset:site.codeType}))
 	fetch(config.discordWebhook, {
 		method: 'POST',
 		headers: {
@@ -40,3 +44,7 @@ setInterval(async() => {
 		})
 	})
 }, config.interval)
+
+function getRandomNum(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}

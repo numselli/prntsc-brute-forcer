9766c51a886f94dbca96efa8151d7fdc96f87421
numselli <58607232+numselli@users.noreply.github.com>
2021-09-13 09:44:35 -0700

    Update index.js to allow file extentions

--- index.js
+++ index.js
@@ -1,46 +1,19 @@
-const fetch = require('node-fetch'), randomstring = require('randomstring'), config = require('./config.json');
-
-const sites = [
-	{
-		"url": "https://prnt.sc/++code++",
-		"codeLen": 6,
-		"minCodeLen": 5,
-		"codeType": "alphanumeric",
-		"lowerCase": true
-	},
-	{
-		"url": "https://postimg.cc/++code++",
-		"codeLen": 8,
-		"minCodeLen": 7,
-		"codeType": "alphanumeric",
-		"lowerCase": false
-	},
-	{
-		"url": "https://imgpile.com/i/++code++",
-		"codeLen": 6,
-		"minCodeLen": 6,
-		"codeType": "alphanumeric",
-		"lowerCase": false
-	},
-	{
-		"url": " https://ibb.co/++code++",
-		"codeLen": 7,
-		"minCodeLen": 2,
-		"codeType": "alphanumeric",
-		"lowerCase": false
-	}
-]
+const fetch = require('node-fetch'), randomstring = require('randomstring'),
+config = require('./config.json'), sites = require('./sites.json')
 
 setInterval(async() => {
-	const site = sites[getRandomNum(0, Object.keys(sites).length)];
+	const site = sites.siteList[getRandomNum(0, Object.keys(sites.siteList).length)];
+
 	const siteURL = site.url.replace('++code++', randomstring.generate({length: getRandomNum(site.minCodeLen, site.codeLen), charset:site.codeType}))
+	const fetchURL = site.addFIleEXT ? siteURL.replace("++ext++", site.availableEXT[getRandomNum(0, site.availableEXT.length)]) : siteURL
+
 	fetch(config.discordWebhook, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
 		},
 		body: JSON.stringify({
-			"content": site.lowerCase ? siteURL.toLowerCase() : siteURL
+			"content": site.lowerCase ? fetchURL.toLowerCase() : fetchURL
 		})
 	})
 }, config.interval)

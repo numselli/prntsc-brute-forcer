ffaf1b021df3a015a4042467462fa847a97a181b
numselli <58607232+numselli@users.noreply.github.com>
2021-09-13 10:46:28 -0700

    only send images with 200 responce

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const fetch = require('node-fetch'), randomstring = require('randomstring'),
-config = require('./config.json'), sites = require('./sites.json')
+config = require('./config.json'), sites = require('./sites.json');
 
 setInterval(async() => {
 	const site = sites.siteList[getRandomNum(0, Object.keys(sites.siteList).length)];
 
-	const siteURL = site.url.replace('++code++', randomstring.generate({length: getRandomNum(site.minCodeLen, site.codeLen), charset:site.codeType}))
-	const fetchURL = site.addFIleEXT ? siteURL.replace("++ext++", site.availableEXT[getRandomNum(0, site.availableEXT.length)]) : siteURL
+	const siteURL = site.url.replace('++code++', randomstring.generate({length: getRandomNum(site.minCodeLen, site.codeLen), charset:site.codeType}));
+	const fetchURL = site.addFIleEXT ? siteURL.replace("++ext++", site.availableEXT[getRandomNum(0, site.availableEXT.length)]) : siteURL;
+
+	const fetchedSite = await fetch(fetchURL);
+	if (fetchedSite.status != 200) return;
 
 	fetch(config.discordWebhook, {
 		method: 'POST',
